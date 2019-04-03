34252caf08a0592a976c7d33b41aad40656ded21
Erik Lissel <erik@lissel.net>
2019-04-04 01:28:07 +0200

    Post2 + RSS

--- build.js
+++ build.js
@@ -1,70 +1,160 @@
 const fs = require("fs");
 const path = require("path");
+const moment = require("moment");
 
-let articleTwigs = fs.readdirSync("articles");
+build();
 
-let articleStatus = {};
+function build() {
+    let articleTwigs = fs.readdirSync("articles");
 
-for(let twigName of articleTwigs) {
-    let localPath = path.join("articles",twigName);
-    let stat = fs.statSync(localPath);
-    articleStatus[twigName] = {
-        name: twigName,
-        localPath: localPath,
-        lastEdit: stat.mtimeMs,
+    let articleStatus = {};
+
+    for (let twigName of articleTwigs) {
+        let localPath = path.join("articles", twigName);
+        let stat = fs.statSync(localPath);
+        articleStatus[twigName] = {
+            name: twigName,
+            localPath: localPath,
+            lastEdit: stat.mtimeMs,
+        };
+    }
+
+    let articleTemplate = fs.readFileSync("articleTemplate.html", 'utf8');
+
+    if (!fs.existsSync("dist"))
+        fs.mkdirSync("dist");
+
+    let articleMetas = [];
+    for (let article of Object.values(articleStatus))
+        articleMetas.push(generateArticle(article, articleTemplate));
+
+    articleMetas.sort((a, b) => a.date > b.date ? -1 : (b.date < a.date ? 1 : 0));
+
+    generateIndexHtml(articleMetas);
+    generateRssFeed(articleMetas);
+
+    copyStaticFiles();
+}
+
+function generateArticle(article, articleTemplate) {
+    let raw = fs.readFileSync(article.localPath, 'utf8');
+    let { content, title, meta, date, tags } = parseArticleTwig(raw, article.name);
+    let articleHtml = articleTemplate
+        .replace(/#title#/g, title) // using /g because of <title> and <h1>
+        .replace(/#content#/, content)
+        .replace(/#created#/, generateDateHtml(date))
+        .replace(/#meta#/, meta)
+        .replace(/#tags#/, "" /*generateTagsHtml(tags)*/)
+        ;
+
+    fs.writeFileSync("dist/" + article.name, articleHtml);
+
+    return {
+        title: title,
+        meta: meta,
+        date: date,
+        tags: tags,
+        fileName: article.name
     };
 }
 
-let mainFile = fs.readFileSync("main.html",'utf8');
-
-if(!fs.existsSync("dist"))
-    fs.mkdirSync("dist");
-
-let generatedStatus = [];
-for(let article of Object.values(articleStatus)) {
-    generateArticle(article);
+function generateIndexHtml(articleMetas) {
+    let indexHtml = fs.readFileSync("index.html", 'utf8');
+    let articleListHtml = (`
+        <section>
+            ${articleMetas.map(x => `<div class="article-brief">
+                ${/*<div>${generateTagsHtml(x.tags)}</div>*/""}
+                <h2><a href="${x.fileName}">${x.title}</a></h2>
+                <div>${generateDateHtml(x.date)}</div>
+                <p><a href="${x.fileName}">${x.meta}</a></p>
+            </div>`).join("")}
+        </section>
+    `);
+    indexHtml = indexHtml.replace(/#article-list#/, articleListHtml);
+    fs.writeFileSync("dist/index.html", indexHtml);
 }
 
-function generateArticle(article) {
-    let raw = fs.readFileSync(article.localPath, 'utf8');
-    let {content, title, meta} = parseArticleTwig(raw, article.name);
-    let articleHtml = mainFile
-        .replace(/#title#/,"<title>"+title+" - Lissel's code blog</title>")
-        .replace(/#content#/,content);
-    
-    fs.writeFileSync("dist/"+article.name, articleHtml);
-
-    generatedStatus.push({
-        title: title,
-        meta: meta
-    });
+function copyStaticFiles() {
+    let staticFiles = fs.readdirSync("static");
+    for (let fileName of staticFiles) {
+        let sourcePath = path.join("static", fileName);
+        let destinationPath = path.join("dist", fileName);
+        fs.copyFileSync(sourcePath, destinationPath);
+    }
 }
 
 function parseArticleTwig(twig, twigName) {
     function parseField(fieldName) {
-        let fieldString = "#"+fieldName+"#";
+        let fieldString = "#" + fieldName + "#";
         let pos = twig.indexOf(fieldString);
-        if(pos === -1)
-            throw "Could not parse expected field "+fieldString+" for "+twigName;
-        let endPos = twig.indexOf("\n",pos+fieldString.length);
-        if(endPos === -1)
-            throw "Could not find end of line (\\n) while parsing field "+fieldString+" for "+twigName;
-        return twig.substring(pos+fieldString.length,endPos).trim();
+        if (pos === -1)
+            throw "Could not parse expected field " + fieldString + " for " + twigName;
+        let endPos = twig.indexOf("\n", pos + fieldString.length);
+        if (endPos === -1)
+            throw "Could not find end of line (\\n) while parsing field " + fieldString + " for " + twigName;
+        return twig.substring(pos + fieldString.length, endPos).trim();
     }
 
     let contentStart = 0;
-    while(true) {
-        let lineEnd = twig.indexOf("\n",contentStart);
-        if(lineEnd === -1)
-            throw "No content found while parsing "+twigName;
-        if(twig.substring(contentStart,lineEnd).indexOf("#")===-1)
+    while (true) {
+        let lineEnd = twig.indexOf("\n", contentStart);
+        if (lineEnd === -1)
+            throw "No content found while parsing " + twigName;
+        if (twig.substring(contentStart, lineEnd).indexOf("#") === -1)
             break;
-        contentStart = lineEnd+1;
+        contentStart = lineEnd + 1;
     }
 
     return {
         content: twig.substring(contentStart),
         title: parseField("title"),
-        meta: parseField("meta")
+        meta: parseField("meta"),
+        date: parseField("created"),
+        tags: parseField("tags").split(",").map(x => x.trim()),
     };
+}
+
+function generateTagsHtml(tags) {
+    return (
+        '<span class="tag-container">' +
+        tags.map(tag => '<span class="tag">' + tag + '</span>').join("") +
+        '</span>');
+}
+
+function generateDateHtml(date) {
+    return '<span class="article-date">' + date + '</span>';
+}
+
+function generateRssFeed(articleMetas) {
+    let now = getTimestamp();
+    let rss = `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+    <channel>
+        <title>Lissel's code blog</title>
+        <description>The article feed containing snippets about c0de.</description>
+        <link>http://www.lissel.net/</link>
+        <lastBuildDate>${now}</lastBuildDate>
+        <pubDate>${now}</pubDate>
+        <ttl>1800</ttl>${articleMetas.map(article => `
+        <item>
+            <title>${article.title}</title>
+            <description>${article.meta}</description>
+            <link>http://www.lissel.net/${article.fileName}</link>
+            <guid isPermaLink="false">http://www.lissel.net/${article.fileName}</guid>
+            <pubDate>${getTimestamp(article.date)}</pubDate>
+        </item>`).join("")}
+    </channel>
+</rss>`;
+    fs.writeFileSync(path.join("dist", "lissel.rss"), rss);
+}
+
+/*
+    arg can be "YYYY-MM-DD" or nothing=now
+*/
+function getTimestamp(arg) {
+    if (typeof (arg) === "string")
+        arg = new Date(arg)
+    else
+        arg = arg || new Date();
+    return moment(arg).format('ddd, DD MMM YYYY HH:mm:ss ZZ');
 }
---
+++ static/common.js
@@ -0,0 +1,70 @@
+(function () {
+    if (document.readyState === "interactive" || document.readyState === "complete")
+        init();
+    else
+        document.addEventListener("DOMContentLoaded", init);
+
+    function init() {
+        try { initTabs(); } catch(e) { console.error(e); }
+        try { styleTagBackgrounds(); } catch(e) { console.error(e); }
+    }
+
+    // === TAGS ===
+    function styleTagBackgrounds() {
+        var tagElements = document.querySelectorAll(".tag-container .tag");
+        var backgrounds = [
+            ["javascript", "tomato"],
+            ["functional", "dodgerblue"]
+        ];
+        for (var element of tagElements) {
+            var background = backgrounds.find(function (bg) {
+                return bg[0] === element.innerHTML.trim();
+            });
+            if (background) {
+                element.style.backgroundColor = background[1];
+            }
+        }
+    }
+
+    // === TABS ===
+    function initTabs() {
+        function selectButton(element) {
+            element.style.fontWeight = "bold"; element.style.textDecoration = "underline";
+        }
+        function deselectButton(element) {
+            element.style.fontWeight = "normal"; element.style.textDecoration = "none";
+        }
+
+        var tabPanes = document.querySelectorAll(".tab-pane");
+        for (var i = 0; i < tabPanes.length; i++) {
+            (function () {
+                var tabPane = tabPanes[i];
+                var tabList = document.createElement("div");
+                var tabNodes = tabPane.querySelectorAll(".tab");
+                var hideTabs = function () { for (var n = 0; n < tabNodes.length; n++) tabNodes[n].style.display = "none"; };
+                var deselectButtons = function () {
+                    var button = tabList.firstElementChild;
+                    do {
+                        deselectButton(button);
+                    } while (button = button.nextElementSibling)
+                };
+
+                for (var j = 0; j < tabNodes.length; j++) {
+                    (function () {
+                        var tab = tabNodes[j];
+                        var label = document.createElement("span");
+                        label.innerHTML = tab.getAttribute("data-title");
+                        label.classList = "tab-button";
+                        label.addEventListener("click", function () { hideTabs(); tab.style.display = "block"; deselectButtons(); selectButton(label); });
+                        tabList.appendChild(label);
+                    })();
+                }
+
+                hideTabs();
+                tabNodes[0].style.display = "block";
+                selectButton(tabList.firstChild);
+                tabPane.insertBefore(tabList, tabPane.firstChild);
+            })();
+        }
+    }
+})();
